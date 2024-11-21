0b264b494566f63b3338397ad5a1b394a4bbef34
Vlaad1955 <wylf1312@gmail.com>
2024-11-22 00:23:01 +0200

    add BaseComponent1

--- src/app/service/movieId.service.tsx
+++ src/app/service/movieId.service.tsx
@@ -2,7 +2,7 @@ import {urls} from "@/app/constant/urls";
 import {AllinfoModel} from "@/app/models/AllinfoModel";
 
 
-export  async function getToId (movieId): Promise<AllinfoModel[]> {
+export  async function getToId (movieId): Promise<AllinfoModel> {
     const response = await fetch(`${urls.baseURL}/movie/${movieId}?api_key=${urls.apiKey}&language=uk-UA`);
     return response.json();
 }
