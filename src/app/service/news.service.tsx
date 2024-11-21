22d1c97ef3bf9b778bc7fc8612d5a3116b8500fe
Vlaad1955 <wylf1312@gmail.com>
2024-11-22 00:17:10 +0200

    add BaseComponent1

--- src/app/service/news.service.tsx
+++ src/app/service/news.service.tsx
@@ -2,7 +2,7 @@ import {urls} from "@/app/constant/urls";
 import {NewsModel} from "@/app/models/NewsModel";
 
 
-export async function newsService(): Promise<NewsModel[]> {
+export async function newsService(): Promise<NewsModel> {
     const response = await fetch(`${urls.latestMovie}`);
     return await response.json();
 }
